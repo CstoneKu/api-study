30a1f7fad2d66dfc5c5b679029aefa01284c5b65
jeongeun1 <wjddms6188@gmail.com>
2024-07-23 17:51:34 +0900

    Feat: 09 JWT ~281

---
+++ ParkJeongeun/_09_JWT_login_logout/src/main/java/com/sh/app/entity/Authority.java
@@ -0,0 +1,6 @@
+package com.sh.app.entity;
+
+public enum Authority {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ ParkJeongeun/_09_JWT_login_logout/src/main/java/com/sh/app/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sh.app.repository;
+
+import com.sh.app.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
